79bed4b97708ccf0e71df51065b21a10719ff1ee
jason243 <33056717+jason243@users.noreply.github.com>
2018-05-15 20:54:28 +0100

    needs a change in direction
    need to change - rather than scrape the entire table, just scrape the date and closing price.

--- scraper test.py
+++ scraper test.py
@@ -5,25 +5,23 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import csv
 
-# test link:  https://markets.ft.com/data/funds/tearsheet/historical?s=GB00BHZK8872:GBX
-
-
-link = input('enter a ft fund data link: ') # grabs link
+link = "https://markets.ft.com/data/funds/tearsheet/historical?s=GB00BHZK8872:GBX" # grabs link
 a = requests.get(link) # saves page
 myfile = a.text # converts page to text file
 b = BeautifulSoup(myfile, 'html.parser') # parses html
 
-c = b.find('section' , attrs = {'class' : 'mod-main-content'} ) # finds date
+c = b.find('div' , attrs = {'class' : 'mod-ui-table--freeze-pane__container'} ) # finds date
 # d = b.find('td', attrs={'class': ''} ) # finds corresponding closing price
 spreadsheet = c.text.strip()
+sort = csv.reader(spreadsheet, delimiter = '-')
 # cprice = d.text.strip() # strip() is used to remove starting and trailing
 
-print(spreadsheet)
+print(sort)
 # print(cprice)
 
 with open('index.csv' , 'w' , newline = '' ) as csv_file:
-    writer = csv.writer(csv_file , lineterminator = '----' )
-    writer.writerow([spreadsheet])
+    writer = csv.writer(csv_file )
+    writer.writerow([sort])
 
 # print(myfile)
 # b = open("test.txt", myfile)
