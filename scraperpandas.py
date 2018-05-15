fef4d510f76e725c27c0eee36245e905145ac90f
jason243 <33056717+jason243@users.noreply.github.com>
2018-05-15 22:00:51 +0100

    Update scraperpandas.py

--- scraperpandas.py
+++ scraperpandas.py
@@ -7,6 +7,9 @@ import csv
 import pandas as pd
 import lxml
 
+# test link : https://markets.ft.com/data/funds/tearsheet/historical?s=GB00BHZK8872:GBX
+
+
 link = input('enter a ft fund data link: ') # grabs link
 df_list = pd.read_html(link)
 df_list[0].to_csv('test.csv')
