c89ff324c4b7cfa0731ade7ddff9a6b5ba1107ff
jason243 <33056717+jason243@users.noreply.github.com>
2018-05-18 22:25:32 +0100

    can add multiple funds
    not sure if it works

--- scraperpandas.py
+++ scraperpandas.py
@@ -7,6 +7,17 @@ import csv
 import pandas as pd
 import lxml
 
-link = input('enter a ft fund data link: ') # grabs link
-df_list = pd.read_html(link)
-df_list[0].to_csv('test.csv')
+
+ISINlist = []
+while True:
+    print('enter an ISIN or nothing to stop: ')
+    ISIN = input()
+    link = 'https://markets.ft.com/data/funds/tearsheet/historical?s=' + ISIN
+    df_list = pd.read_html(link)
+    df_list[0].to_csv('test.csv')
+    if link == '':
+        break
+    ISINlist = ISINlist + [ISIN]
+print('The cat names are:')
+for ISIN in ISINlist:
+    print(' ' + ISIN)
